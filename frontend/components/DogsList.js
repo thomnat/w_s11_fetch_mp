9a023c6e13d790176aeaf27daec645fc426254f8
Natalie Thompson <nataliethompson2314@gmail.com>
2024-05-03 10:53:12 -0700

    completed

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,17 +1,44 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export default function DogsList() {
+export default function DogsList( { dogs, getDogs, setCurrentDog }) {
+  const navigate = useNavigate()
+  
+  const editDog = id => {
+    //set that id as the current id
+    setCurrentDog(id)
+    //navigate to the form
+    navigate('form')
+  }
+
+  const deleteDog = id => {
+    fetch(`/api/dogs/${id}`, { method: 'DELETE' })
+      .then(res => {
+        if (!res.ok) throw new Error("Problem DELETEing dog")
+        getDogs()
+        setCurrentDog(null)
+      })
+      .catch(err => console.error(err))
+  }
+
+
+
+
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        <li>
-          Fido, Bulldog, NOT adopted
-          <div>
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
-        </li>
+        {
+          dogs.map(dog => (
+            <li key={dog.id}>
+              {dog.name}, {dog.breed}, {dog.adopted ? '' : 'NOT '}adopted
+             <div>
+                <button onClick={() => editDog(dog.id)}>Edit</button>
+                <button onClick={() => deleteDog(dog.id)}>Delete</button>
+              </div>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
